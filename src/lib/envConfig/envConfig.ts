877ce69342f1822b4654385862a8ef8987c4e330
Scriptscrypt <tirumaladasa@gmail.com>
2024-07-24 00:48:30 +0530

    Feat: CSS and Styling fixes for page.tsx UI

--- src/lib/envConfig/envConfig.ts
+++ src/lib/envConfig/envConfig.ts
@@ -2,3 +2,4 @@ export const envMongoUri = process.env.NEXT_PUBLIC_MONGODB_URI;
 export const envTelegramBotToken = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
 export const envTelegramChatId = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID;
 export const envEnviroment = process.env.NEXT_PUBLIC_ENVIROMENT;
+export const envAppUrl = process.env.NEXT_PUBLIC_APP_URL;
