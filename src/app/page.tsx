20ee633d828f4d9e56c2c7b487f092bf1a5f450b
Scriptscrypt <tirumaladasa@gmail.com>
2024-07-25 02:51:55 +0530

    Add: Example Req

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 // import { headers } from "next/headers";
 
 export default function Home() {
-  
   // const headersList = headers();
   // const fullUrl = headersList.get("referer") || "";
   const fullUrl = "https://Blinktochat.fun/";
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Blinktochat.fun - Personalized group chat to only those who blinked you on X.
+          Blinktochat.fun - Personalized group chat to only those who blinked
+          you on X.
           {/* <code className="font-mono font-bold">src/app/page.tsx</code> */}
         </p>
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -48,7 +48,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
         <a
-          href={`https://dial.to/devnet?action=solana-action:${fullUrl}api/actions/start`}
+          href={`https://dial.to/devnet?action=solana-action:${fullUrl}api/actions/start/-1002232395603/39G4S57hEMsbD1npzi22heiEvjAHnnTG3ixciDHozcNj`}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
